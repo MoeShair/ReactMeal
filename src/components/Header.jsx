a9ebd74913505a01460d84994e58221da1f3fba9
MoeShair <hamoda.tam@gmail.com>
2024-07-24 10:44:33 +0300

    ReactMeal

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import logoImg from '../assets/logo.jpg'
+import Button from "./UI/Button.jsx";
+
+export default function Header({}){
+    return(
+      <>
+          <header id='main-header'>
+              <div id='title'>
+                  <img src={logoImg} alt='sds'/>
+                  <h1>ReactFood</h1>
+              </div>
+              <nav>
+                  <Button textOnly> Cart (0)</Button>
+              </nav>
+          </header>
+      </>
+    );
+}
